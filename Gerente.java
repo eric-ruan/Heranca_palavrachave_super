5e9f0e3f4f697a9e9271b74263d4414cdadc2681
Éric <ericrss2021@gmail.com>
2021-11-09 16:40:21 -0300

    Programa pronto

--- Gerente.java
+++ Gerente.java
@@ -8,6 +8,6 @@ public class Gerente extends Funcionario {
     }
     
     public double getBonificacao(){
-        return this.getSalario() * 0.15;
+        return super.getSalario() + 1000.0;
     }
 }
